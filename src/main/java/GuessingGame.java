498edb8bc480dcb46fb006f02e7ed895c74cb1a0
Isabel Martinez de Rituerto <imderituerto@Isabels-MacBook-Pro.local>
2022-11-15 16:57:48 +0100

    Added Lab 2

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+
+        // Exercise for ...
+
+        // Program a 'secret number'
+        // Ask user to guess the number
+        // If guess is lower, print "Too low! Try again."
+        // If guess is higher, print "Too high! Try again."
+        // Create a loop so game runs until correct number (?)
+
+        long x = 666;
+
+        // Loop: while(true){} + break;
+
+        while(true) {
+            // Prompt the use for input
+            System.out.println("Guess a number between 1 and 1000:");
+
+            // Collect user input 1
+            Scanner number = new Scanner(System.in);
+            int playerGuess = number.nextInt();
+
+//            // Collect user input 2
+//            Scanner scanner = new Scanner(System.in); // Prompts user for input (?)
+//            String guess = scanner.nextLine(); // Allows user to provide input and press return.
+//            int playerGuess1 = Integer.parseInt(guess); // Transforming "string" input from user into an "integer" type.
+
+            if (playerGuess == x) {
+                System.out.println("That's right!");
+                break;
+            } else if (playerGuess > x) {
+                System.out.println("Too high! Try again."); //prints true boolean
+            } else if (playerGuess < x) {
+                System.out.println("Too low! Try again."); //prints true boolean
+            }
+        }
+    }
+}
---
+++ src/main/java/WhatShouldIWear.java
@@ -0,0 +1,32 @@
+public class WhatShouldIWear {
+    public static void main(String[] args) {
+
+        // Exercise for logical and conditional operators
+
+        System.out.println("What is the weather like?");
+
+        System.out.println("Rainy, sunny, or snowy?");
+
+        System.out.println("Cold, warm, or hot?");
+
+        String r = "rainy";
+        String s = "sunny";
+        String sn = "snowy";
+
+        int cold = 0;
+        int warm = 16;
+        int hot = 26;
+
+
+//        if(r) {
+//            System.out.println("Bring a jacket and an umbrella.");
+//        } else if(s) {
+//            System.out.println("Just bring an umbrella.");
+//        } else(sn) {
+//            System.out.println("Wear shorts, but bring an umbrella!");
+//        }
+
+        System.out.println(cold);
+
+    }
+}
